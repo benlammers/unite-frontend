246bc06055016552a32001e4e37382ec98da3b93
benlammers <21benlammers@gmail.com>
2020-11-29 00:41:57 -0500

    Hardcode Backend Url

--- src/util/axiosRequest.js
+++ src/util/axiosRequest.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 console.log(process.env.NODE_ENV)
 
 const axiosRequest = axios.create({
-   baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/' : process.env.REACT_APP_BACKEND_URL,
+   // baseURL: process.env.NODE_ENV === 'development' ? 'http://127.0.0.1:8000/' : process.env.REACT_APP_BACKEND_URL,
+   baseURL: 'https://uniteapi.herokuapp.com/',
    responseType: 'json',
 })
 
