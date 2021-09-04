82166c939659e4afc5b92198dcaca8b3765fef7b
Ben Lammers <21benlammers@gmail.com>
2021-09-04 12:30:11 -0400

    Update Registry.jsx

--- src/components/Home/Registry/Registry.jsx
+++ src/components/Home/Registry/Registry.jsx
@@ -10,11 +10,7 @@ const Registry = () => {
             Please know that your presence at our wedding is present enough! However, for friends and family who have
             been asking for gift ideas, here is a link to our online registry
          </span>
-         <a
-            className='button button--white text-primary'
-            href='#registry'
-            rel='noopener noreferrer'
-            target='_blank'>
+         <a className='button button--white text-primary' href='#registry'>
             View
          </a>
       </div>
